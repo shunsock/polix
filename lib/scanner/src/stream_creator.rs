11404e06e5264ea46cfa5dbe2b851c259044ba95
Shun <s.tsuchiya.business@gmail.com>
2025-03-09 22:07:22 +0900

    feature: source code generator passed test

--- lib/scanner/src/stream_creator.rs
+++ lib/scanner/src/stream_creator.rs
@@ -3,33 +3,33 @@ use core::source_code::Line;
 use core::source_code::Position;
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct StreamCreator {
-    source_code_chars: Vec<char>,
-    stream: Vec<SourceCodeCharacter>,
+pub struct SourceStreamGenerator {
+    rest_source_code: Vec<char>,
+    processed: Vec<SourceCodeCharacter>,
     line: u32,
     position: u32,
 }
 
-impl StreamCreator {
+impl SourceStreamGenerator {
     pub fn new(
-        source_code: Vec<char>,
-        stream: Vec<SourceCodeCharacter>,
+        rest_source_code: Vec<char>,
+        processed: Vec<SourceCodeCharacter>,
         index: Option<u32>,
         position: Option<u32>
-    ) -> StreamCreator {
+    ) -> SourceStreamGenerator {
         match (index, position) {
-            (Some(i), Some(p)) => {
-                StreamCreator {
-                    source_code_chars: source_code,
-                    stream,
-                    line: i,
+            (Some(l), Some(p)) => {
+                SourceStreamGenerator {
+                    rest_source_code,
+                    processed,
+                    line: l,
                     position: p,
                 }
             }
             _ => {
-                StreamCreator {
-                    source_code_chars: source_code,
-                    stream,
+                SourceStreamGenerator {
+                    rest_source_code,
+                    processed,
                     line: 1,
                     position: 1,
                 }
@@ -37,14 +37,16 @@ impl StreamCreator {
         }
     }
 
-    pub fn create_stream(&self) {
-        match self.source_code_chars.len() {
-            0 => {}
+    pub fn generate(&self) -> Self {
+        match self.rest_source_code.len() {
+            0 => {
+                self.clone()
+            }
             _ => {
                 // Get the first character from the source code
                 // This unwrap is ok, because we are sure that the vec is not empty
-                let mut rest_chars: Vec<char> = self.source_code_chars.clone();
-                let first_char: char = rest_chars.pop().unwrap();
+                let mut rest_chars: Vec<char> = self.rest_source_code.clone();
+                let first_char: char = rest_chars.remove(0);
 
                 // create a new SourceCodeCharacter
                 // These unwraps are ok, because we are sure that the line and position are greater than 0
@@ -55,27 +57,84 @@ impl StreamCreator {
                 );
 
                 // push the new SourceCodeCharacter to the stream
-                let mut updated_stream: Vec<SourceCodeCharacter> = self.stream.clone();
-                updated_stream.push(source_code_char);
+                let mut processed: Vec<SourceCodeCharacter> = self.processed.clone();
+                processed.push(source_code_char);
 
                 // if the first character is a newline character, increment the line and reset the position
                 if first_char == '\n' {
-                    return StreamCreator::new(
+                    return SourceStreamGenerator::new(
                         rest_chars,
-                        updated_stream,
+                        processed,
                         Some(self.line + 1),
                         Some(1),
-                    ).create_stream();
+                    ).generate();
                 }
 
                 // create a new StreamCreator with the rest of the source code
-                StreamCreator::new(
+                SourceStreamGenerator::new(
                     rest_chars,
-                    updated_stream,
+                    processed,
                     Some(self.line),
                     Some(self.position + 1),
-                ).create_stream();
+                ).generate()
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    /// # Test StreamCreator parse a source code without \n
+    /// The source code is `['a', 'b', 'c']`
+    /// The expected result is a stream with 3 SourceCodeCharacter
+    /// 1. SourceCodeCharacter { character: 'a', line: 1, position: 1 }
+    /// 2. SourceCodeCharacter { character: 'b', line: 1, position: 2 }
+    /// 3. SourceCodeCharacter { character: 'c', line: 1, position: 3 }
+    fn test_parse_source_code_without_newline() {
+        // Arrange
+        let source_code: Vec<char> = vec!['a', 'b', 'c'];
+        let stream_creator = SourceStreamGenerator::new(source_code, vec![], None, None);
+
+        // Act
+        let stream_creator: SourceStreamGenerator = stream_creator.generate();
+
+        // Assert
+        let expected: Vec<SourceCodeCharacter> = vec![
+            SourceCodeCharacter::new('a', Line::new(1).unwrap(), Position::new(1).unwrap()),
+            SourceCodeCharacter::new('b', Line::new(1).unwrap(), Position::new(2).unwrap()),
+            SourceCodeCharacter::new('c', Line::new(1).unwrap(), Position::new(3).unwrap()),
+        ];
+
+        assert_eq!(stream_creator.processed, expected);
+    }
+
+    #[test]
+    /// # Test StreamCreator can increment line
+    /// The source code is `['a', '\n', 'b', 'c']`
+    /// The expected result is a stream with 3 SourceCodeCharacter
+    /// 1. SourceCodeCharacter { character: 'a', line: 1, position: 1 }
+    /// 2. SourceCodeCharacter { character: '\n', line: 1, position: 2 }
+    /// 3. SourceCodeCharacter { character: 'b', line: 2, position: 1 }
+    /// 4. SourceCodeCharacter { character: 'c', line: 2, position: 2 }
+    fn test_parse_source_code_with_newline() {
+        // Arrange
+        let source_code: Vec<char> = vec!['a', '\n', 'b', 'c'];
+        let stream_creator = SourceStreamGenerator::new(source_code, vec![], None, None);
+
+        // Act
+        let stream_creator: SourceStreamGenerator = stream_creator.generate();
+
+        // Assert
+        let expected: Vec<SourceCodeCharacter> = vec![
+            SourceCodeCharacter::new('a', Line::new(1).unwrap(), Position::new(1).unwrap()),
+            SourceCodeCharacter::new('\n', Line::new(1).unwrap(), Position::new(2).unwrap()),
+            SourceCodeCharacter::new('b', Line::new(2).unwrap(), Position::new(1).unwrap()),
+            SourceCodeCharacter::new('c', Line::new(2).unwrap(), Position::new(2).unwrap()),
+        ];
+
+        assert_eq!(stream_creator.processed, expected);
     }
 }
